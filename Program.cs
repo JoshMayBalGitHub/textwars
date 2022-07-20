b19939c9913fdc607a4d5cd3719f4862c836f520
JoshMayBalGitHub <joshuamiguelcalara@gmail.com>
2022-07-20 11:37:36 +0000

    Add back old textwars question with a bit of change.

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace textwars
       } 
       else if (cmd == "help") 
       {
-        var text = System.IO.File.ReadAllText("./config/help.txt");
+        var text = File.ReadAllText("./config/help.txt");
         Console.WriteLine(text);
         goto cmderror;
       }
@@ -42,10 +42,73 @@ namespace textwars
         Console.WriteLine(cmd + ": Command not found. Please try again");
         goto cmderror;
       }
+
       Error:
-      Console.WriteLine("In development, sorry about that!");
-      Thread.Sleep(5000);
-      goto cmderror;
+      Thread.Sleep(2000);
+      Console.WriteLine("Do you like school?");
+      Thread.Sleep(1500);
+      Console.WriteLine("Use Yes or No.");
+      var wololo = Console.ReadLine();
+
+      if (wololo == "Yes")
+      {
+        Console.WriteLine("Ok, Suuure....");
+      }
+      else if (wololo == "yes")
+      {
+        Console.WriteLine("ok, suure..");
+      }
+      else if (wololo == "No")
+      {
+        Console.WriteLine("Yeah, Mee Too....");
+      }
+      else if (wololo == "no")
+      {
+        Console.WriteLine("yeah, me to..");
+      }
+      else
+      {
+        Console.WriteLine(wololo + ": Error, not a valid option");
+        Thread.Sleep(1500);
+        goto Error;
+      }
+
+      Error2:
+      Thread.Sleep(2000);
+      Console.WriteLine("Do you want to try again?");
+      Console.WriteLine("Answer Yes or No.");
+      var narwini = Console.ReadLine();
+      if (narwini == "Yes")
+      {
+        Console.WriteLine("Okk, That Is Good!");
+        goto Error;
+      }
+      else if (narwini == "yes")
+      {
+        Console.WriteLine("ok, thats good.");
+        Console.WriteLine("in development.. sorry..");
+        Thread.Sleep(5000);
+        var lol1 = File.ReadAllText("./config/lol.txt");
+        Console.WriteLine(lol1);
+        Thread.Sleep(5000);
+        goto Error;
+      }
+      else if (narwini == "No")
+      {
+        Console.WriteLine("OK Then! Exiting Out!");
+        goto cmderror;
+      }
+      else if (narwini == "no")
+      {
+        Console.WriteLine("NOOOOO! :( OK.....");
+        Console.WriteLine("YOU SHALL BE BANISHED!");
+        Environment.Exit(0);
+      }
+      else
+      {
+        Console.WriteLine("ERROR, try again.");
+        goto Error2;
+      }
     }
   }
 }
